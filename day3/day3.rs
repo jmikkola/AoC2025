2f32e38c8f79fe96282c2f7288f959418e29c873
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-12-03 18:59:16 -0500

    Add part 2

--- day3/day3.rs
+++ day3/day3.rs
@@ -4,17 +4,51 @@ use std::fs;
 fn main() {
     let content = get_content(get_arg());
     part1(&content);
+    part2(&content);
 }
 
 fn part1(input: &str) {
     let mut total = 0;
     for line in input.split('\n') {
         let result = joltage(line);
-        println!("{} {}", line, result);
         total += result;
     }
 
-    println!("total: {}", total);
+    println!("part 1: {}", total);
+}
+
+fn part2(input: &str) {
+    let mut total = 0;
+    for line in input.split('\n') {
+        let result = joltage2(line);
+        total += result;
+    }
+
+    println!("part 2: {}", total);
+}
+
+fn joltage2(line: &str) -> u64 {
+    let chars: Vec<char> = line.chars().collect();
+    find_max_joltage(12, 0, &chars)
+}
+
+fn find_max_joltage(n: usize, so_far: u64, chars: &[char]) -> u64 {
+    if n == 0 {
+        return so_far;
+    }
+
+    let mut best = 0;
+    let mut rest_chars = &chars[1..];
+
+    for i in 0..chars.len()-n+1 {
+        let char_value = chars[i] as u64 - '0' as u64;
+        if char_value > best {
+            best = char_value;
+            rest_chars = &chars[i+1..];
+        }
+    }
+
+    find_max_joltage(n - 1, so_far * 10 + best, rest_chars)
 }
 
 fn joltage(line: &str) -> u32 {
