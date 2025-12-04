a617db9a8a778eeee4ebc54836de221e3e59005e
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-12-04 09:15:11 -0500

    Part 2

--- day4/day4.rs
+++ day4/day4.rs
@@ -4,6 +4,7 @@ use std::fs;
 fn main() {
     let content = get_content(get_arg());
     part1(&content);
+    part2(&content);
 }
 
 fn get_arg() -> String {
@@ -50,6 +51,50 @@ fn part1(text: &str) {
             }
         }
         // println!("");
+    }
+
+    println!("Rolls: {}", n_rolls);
+}
+
+
+fn part2(text: &str) {
+    let mut grid = to_grid(text);
+    let mut n_rolls = 0;
+
+    loop {
+        let mut to_remove = vec![];
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == '@' {
+                    let min_i = if i > 0 { i - 1 } else { 0 };
+                    let min_j = if j > 0 { j - 1 } else { 0 };
+                    let max_i = if i < grid.len() - 1 { i + 1 } else { i };
+                    let max_j = if j < grid.len() - 1 { j + 1 } else { j };
+
+                    let mut n_neighbors = 0;
+                    for ii in min_i..max_i+1 {
+                        for jj in min_j..max_j+1 {
+                            if !(ii == i && jj == j) && grid[ii][jj] == '@' {
+                                n_neighbors += 1;
+                            }
+                        }
+                    }
+
+                    if n_neighbors < 4 {
+                        n_rolls += 1;
+                        to_remove.push((i, j));
+                    }
+                }
+            }
+        }
+
+        if to_remove.is_empty() {
+            break;
+        }
+
+        for (i, j) in to_remove.iter() {
+            grid[*i][*j] = '.';
+        }
     }
 
     println!("Rolls: {}", n_rolls);
