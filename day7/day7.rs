e7819fb36b96139e954604d763fbdadee46f430d
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-12-07 08:17:07 -0500

    Add day 7 part 2

--- day7/day7.rs
+++ day7/day7.rs
@@ -1,10 +1,12 @@
 use std::env;
 use std::fs;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let content = get_content(get_arg());
     part1(&content);
+    part2(&content);
 }
 
 fn get_arg() -> String {
@@ -28,7 +30,7 @@ fn part1(input: &str) {
     let mut beams = HashSet::new();
     beams.insert(start_beam);
 
-    println!("{}", chars[0].iter().collect::<String>());
+    // println!("{}", chars[0].iter().collect::<String>());
     for row in chars.iter().skip(1) {
         let mut temp_row = row.clone();
         let mut next_beams = HashSet::new();
@@ -51,9 +53,52 @@ fn part1(input: &str) {
             }
         }
 
-        println!("{}", temp_row.iter().collect::<String>());
+        // println!("{}", temp_row.iter().collect::<String>());
         beams = next_beams;
     }
 
     println!("part 1: {}", splits);
+}
+
+fn part2(input: &str) {
+    let chars: Vec<Vec<char>> = input.split('\n')
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let start_beam = chars[0].iter().position(|&c| c == 'S').unwrap();
+    let mut beams: HashMap<usize, u64> = HashMap::new();
+    beams.insert(start_beam, 1);
+
+    for row in chars.iter().skip(1) {
+        let mut to_insert = vec![];
+        for (&beam, &count) in beams.iter() {
+            if row[beam] == '.' {
+                to_insert.push((beam, count));
+            } else if row[beam] == '^' {
+                let lower = beam.checked_sub(1);
+                if let Some(lower_beam) = lower {
+                    to_insert.push((lower_beam, count));
+                }
+                let upper = beam + 1;
+                if upper < row.len() {
+                    to_insert.push((upper, count));
+                }
+            }
+        }
+
+        let mut next_beams: HashMap<usize, u64> = HashMap::new();
+        for (beam, count) in to_insert.iter() {
+            let existing = next_beams.get(beam).cloned().unwrap_or(0);
+            next_beams.insert(*beam, *count + existing);
+        }
+
+        beams = next_beams;
+    }
+
+    let mut splits = 0;
+    for (_, &count) in beams.iter() {
+        splits += count;
+    }
+
+    println!("part 2: {}", splits);
 }
