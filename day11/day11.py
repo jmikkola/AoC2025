1bf79ff5a707c8deb80499d8a5f7fb37dded2284
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-12-15 22:45:19 -0500

    Add part 2

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def main(args):
@@ -11,6 +12,7 @@ def main(args):
 
     parsed = parse(lines)
     print('part 1:', part1(parsed))
+    print('part 2:', part2(parsed))
 
 
 def parse(lines):
@@ -40,6 +42,40 @@ def dfs(graph, node, seen):
         total += dfs(graph, connection, seen)
 
     seen.remove(node)
+    return total
+
+
+def part2(graph):
+    total = 0
+
+    q = [('svr', 1, 0, 0, 0)]
+
+    # The 0th iteration starts with paths of length 0, and the nth iteration
+    # starts at points reached with paths of length n
+    while q:
+        q2 = []
+        for (node, neither, fft, dac, both) in q:
+            for neighbor in graph.get(node, []):
+                if neighbor == 'out':
+                    total += both
+                    continue
+
+                if neighbor == 'fft':
+                    q2.append((neighbor, 0, neither, 0, dac))
+                elif neighbor == 'dac':
+                    q2.append((neighbor, 0, 0, neither, fft))
+                else:
+                    q2.append((neighbor, neither, fft, dac, both))
+
+        dedup = defaultdict(lambda: (0, 0, 0, 0))
+        for (node, neither, fft, dac, both) in q2:
+            (neither_total, fft_total, dac_total, both_total) = dedup[node]
+            dedup[node] = (neither + neither_total, fft + fft_total, dac + dac_total, both + both_total)
+
+        q = []
+        for (node, (n, f, d, b)) in dedup.items():
+            q.append((node, n, f, d, b))
+
     return total
 
 
